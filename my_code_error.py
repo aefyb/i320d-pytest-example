280e75041aaeb8f4650be99308c3db3265fb11ea
aefyb <nguyen.jess@utexas.edu>
2025-03-30 15:33:52 -0500

    Added lines to remove non-digit characters and remove any lingering non-digits afterwards.

--- my_code_error.py
+++ my_code_error.py
@@ -1,17 +1,24 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
+  # removes all non-digit characters
+  phone_num_to_fix = phone_num_to_fix.replace("-", "").replace("(", "").replace(")", "").replace(" ", "")
+
   # can only handle numbers that are exactly 10 digits long
   if (len(phone_num_to_fix) != 10):
     raise ValueError("Can only format numbers that are exactly 10 digits long")
+
+  # checking if the phone number is only digits
+  if not phone_num_to_fix.isdigit():
+    raise ValueError("Phone number can only have digits")
 
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
   four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
-  
-  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-  
+
+  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
+
   return fixed_num
 
 def test_fix_phone_num():
@@ -29,9 +36,8 @@ def test_two_fix_phone_num():
         if not "555-442-98761".isdigit():
             raise ValueError("Phone number must contain only digits")
         fix_phone_num("555-442-98761")
-    
+
     with pytest.raises(ValueError):
         if not "(321) 654 3333".isdigit():
             raise ValueError("Phone number must contain only digits")
         fix_phone_num("(321) 654 3333")
-
